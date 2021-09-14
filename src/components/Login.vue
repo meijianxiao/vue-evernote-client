a8b58f21d6ff360b5bc4c625caf6708f3db40761
meijianxiao <1003409251@qq.com>
2021-09-14 10:38:59 +0800

    接口封装

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -146,7 +146,7 @@ export default {
 
   .main {
     flex: 1;
-    background: #36bc64 url(//cloud.hunger-valley.com/17-12-13/38476998.jpg-middle) center center no-repeat;
+    background: #36bc64 url(http://cloud.hunger-valley.com/17-12-13/38476998.jpg-middle) center center no-repeat;
     background-size: contain;
   }
 
